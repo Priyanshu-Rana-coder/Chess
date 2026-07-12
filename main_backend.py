be906a3cf3d0d6915847ef8b41851aa4971a4c26
Priyanshu-Rana-coder <ranapriyanshu741@gmail.com>
2026-07-13 02:31:08 +0530

    completed the login page

--- main_backend.py
+++ main_backend.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI, WebSocket
-from act import Game
-from rooms import Room
+from fastapi import FastAPI, WebSocket, Depends, HTTPException
+from sqlalchemy.orm import Session
+from User.database import get_db
+from User.schemas import LoginRequest, UserResponse
+from User.crud import login_user
 app=FastAPI()
 rooms={}
 @app.websocket("/ws")
@@ -20,3 +22,22 @@ async def websocket(websocket: WebSocket):
             data["y2"]
         )
         await websocket.send_json(response)
+
+@app.post("/login", response_model=UserResponse)
+def login(request: LoginRequest,db: Session = Depends(get_db)):
+    user = login_user(
+        db,
+        request.username,
+        request.password
+    )
+    if user is None:
+        raise HTTPException(
+            status_code=401,
+            detail="Incorrect password"
+        )
+    return UserResponse(
+        username=user.username,
+        wins=user.wins,
+        losses=user.losses,
+        draws=user.draws
+    )
