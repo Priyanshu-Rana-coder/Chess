ab6365b18985a314c56ab2393d00d41e267b9b50
Priyanshu-Rana-coder <ranapriyanshu741@gmail.com>
2026-07-18 01:45:55 +0530

    changed url to the online version

--- User/database.py
+++ User/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+import os
 
-DATABASE_URL = "postgresql://postgres:prash741@localhost/FastAPi"
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(
     autoflush=False,
